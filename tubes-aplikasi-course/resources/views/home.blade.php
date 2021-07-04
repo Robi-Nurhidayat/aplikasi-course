8cd779948e6818c8fefcd4c91cb82cb3160b2953
Robi Nurhidayat <robivanpersie290899@gmail.com>
2021-07-04 19:08:04 +0700

    membuat popular course

--- tubes-aplikasi-course/resources/views/home.blade.php
+++ tubes-aplikasi-course/resources/views/home.blade.php
@@ -21,13 +21,14 @@
     </div>
 </div> --}}
 
+{{-- Hero --}}
 <div class="jumbotron bg-light mx-5 my-5">
     <div class="row">
         <div class="col-md-5">
             <h1>Learn Programming</h1>
             <h1 class="mb-4">Smart For Your Future</h1>
 
-            <p class="lead mb-4">Belajar cerdas seputar programming bersama LearnEnt dan dapatkan sertifikat untuk membangun masa depan anda yang lebih terarah</p>
+            <p class="lead mb-4">Belajar cerdas seputar programming bersama ITCourse dan dapatkan sertifikat untuk membangun masa depan anda yang lebih terarah</p>
 
             <a class="btn btn-primary btn-lg" href="#" role="button">Get started</a>   
             <a class="btn btn-secondary btn-lg ml-5" href="#" role="button">Course List</a>
@@ -40,6 +41,10 @@
 
 </div>
 
+{{-- end of Hero --}}
+
+
+{{-- Card plan --}}
 <div class="jumbotron mx-5">
     <div class="row">
         <div class="col-md-4">
@@ -70,15 +75,76 @@
         </div>
 
         <div class="col-md-4">
-            <div class="card" style="width: 18rem;">
+            <div class="card my-5 bg-secondary text-white" style="width: 20rem; height:90%;">
                 <div class="card-body">
-                  <h5 class="card-title"></h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <h5 class="card-title font-weight-bold">Free <br>What You’ll Get?</h5>
+                  <p> Kesempatan belajar secara gratis</p>
+                  <p>  Akses selama nya</p>
+                  <p>  Ilmu yang bermanfaat</p>
+                  <div class="text-center d-flex justify-content-center">
+                    <a href="#" class="btn btn-primary">View Course List</a>
+                  </div>
+                  
                 </div>
               </div>
         </div>
     </div>    
 </div>
+
+{{-- end of card plan --}}
+
+{{-- card popular course --}}
+
+<div class="jumbotron bg-secondary mx-5 my-5">
+    <h1 class="mx-3">Popular Course</h1>
+    <p class="mx-3 font-weight-bold">Top Course Demand in 2021</p>
+    <div class="row mx-2">
+        <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ URL('images/pi1.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Python Basic</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ URL('images/JS.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ URL('images/react.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ URL('images/lara.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+        </div>
+    </div>
+</div>
+
+{{-- end of popular course --}}
 
 @endsection
