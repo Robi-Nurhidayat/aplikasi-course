0626fbbbfad9ffbdadf8048c1d052b8cec5790d6
Robi Nurhidayat <robivanpersie290899@gmail.com>
2021-07-05 20:32:29 +0700

    menambahkan images

--- tubes-aplikasi-course/resources/views/home.blade.php
+++ tubes-aplikasi-course/resources/views/home.blade.php
@@ -48,7 +48,7 @@
 <div class="jumbotron mx-5">
     <div class="row">
         <div class="col-md-4">
-            <div class="card p-2 bg-dark text-white font-card1" style="width: 20rem;">
+            <div class="card p-2 bg-dark text-white font-card1 shadow rounded" style="width: 20rem;">
                 <div class="card-body">
                   <h5 class="card-title font-weight-bold">Save More <br>
                     With Good Plan </h5>
@@ -60,7 +60,7 @@
         </div>
 
         <div class="col-md-4">
-            <div class="card my-5 bg-secondary text-white" style="width: 20rem; height:90%;">
+            <div class="card my-5 bg-secondary text-white shadow rounded" style="width: 20rem; height:90%;">
                 <div class="card-body">
                   <h5 class="card-title font-weight-bold">Free <br>What You’ll Get?</h5>
                   <p> Kesempatan belajar secara gratis</p>
@@ -75,7 +75,7 @@
         </div>
 
         <div class="col-md-4">
-            <div class="card my-5 bg-secondary text-white" style="width: 20rem; height:90%;">
+            <div class="card my-5 bg-secondary text-white shadow rounded" style="width: 20rem; height:90%;">
                 <div class="card-body">
                   <h5 class="card-title font-weight-bold">Free <br>What You’ll Get?</h5>
                   <p> Kesempatan belajar secara gratis</p>
@@ -99,7 +99,7 @@
     <h1 class="mx-3">Popular Course</h1>
     <p class="mx-3 font-weight-bold">Top Course Demand in 2021</p>
     <div class="row mx-2">
-        <div class="col-md-3">
+        <div class="col-md-3 my-2">
             <div class="card" style="width: 18rem;">
                 <img src="{{ URL('images/pi1.jpg') }}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -110,7 +110,7 @@
             </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-3 my-2">
             <div class="card" style="width: 18rem;">
                 <img src="{{ URL('images/JS.jpg') }}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -121,7 +121,7 @@
               </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-3 my-2">
             <div class="card" style="width: 18rem;">
                 <img src="{{ URL('images/react.png') }}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -132,7 +132,7 @@
               </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-3 my-2">
             <div class="card" style="width: 18rem;">
                 <img src="{{ URL('images/lara.jpg') }}" class="card-img-top" alt="...">
                 <div class="card-body">
@@ -146,5 +146,151 @@
 </div>
 
 {{-- end of popular course --}}
+
+{{-- why LearnIT --}}
+
+<div class="jumbotron bg-dark mx-5 my-5">
+
+    <div class="content-why">
+        <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem; height:300px;">
+            <div class="card-header text-center"><img src="{{ URL('images/waktu.jpg') }}" alt="" class="rounded"></div>
+            <div class="card-body">
+              <h5 class="card-title">Waktu Fleksibel</h5>
+              <p class="card-text">Kelas permanen tanpa ada batas waktu</p>
+            </div>
+        </div>
+
+        <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem;height:300px;">
+            <div class="card-header text-center"><img src="{{ URL('images/uang.jpg') }}" alt="" class="rounded"></div>
+            <div class="card-body">
+              <h5 class="card-title">Biaya Yang Terjangkau</h5>
+              <p class="card-text">Harga yang ditawarkan sangat bersahabat dengan dompet anda.</p>
+            </div>
+        </div>
+
+        <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem;height:300px;">
+            <div class="card-header text-center"><img src="{{ URL('images/mentor.jpg') }}" alt="" class="rounded"></div>
+            <div class="card-body">
+              <h5 class="card-title">Pengajar Professional</h5>
+              <p class="card-text">Pengajar sudah professional di bidang nya</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+{{-- end of LearnIT --}}
+
+{{-- testimonial students --}}
+
+
+<div class="jumbotron bg-dark mx-5 my-5">
+  <div class="tetimonial-header mb-3 ml-4">
+    <h2>Testimonial Students</h2>
+    <h4>Testimonial from students, Our Say : </h4>
+  </div>
+
+  <div class="testi-section">
+      <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem; height:300px;">
+        <div class="card-header text-center">
+          <div class="row">
+              <div class="col-md-4">
+                <img src="{{ URL('images/bagus.jpg') }}" alt="" class="rounded rounded-circle mt-3" style="max-width: 4rem; height:4rem;">
+              </div>
+              <div class="col-md-6 mt-3 ">
+                <p>Bagus Perdana</p>
+                <p>UI Designer</p>
+              </div>
+          </div>
+        </div>
+        <div class="card-body">
+          {{-- <h5 class="card-title">Waktu Fleksibel</h5> --}}
+          <p class="card-text">Penjelasan mudah dimengerti serta kualitas materi nya sangat bagus.</p>
+        </div>
+    </div>
+    
+    <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem; height:300px;">
+      <div class="card-header text-center">
+        <div class="row">
+            <div class="col-md-4">
+              <img src="{{ URL('images/fahrul.jpg') }}" alt="" class="rounded rounded-circle mt-3" style="max-width: 4rem; height:4rem;">
+            </div>
+            <div class="col-md-6 mt-3 ">
+              <p>Raden Fachrul</p>
+              <p>Web Developer</p>
+            </div>
+        </div>
+      </div>
+      <div class="card-body">
+        {{-- <h5 class="card-title">Waktu Fleksibel</h5> --}}
+        <p class="card-text">Penjelasan mudah dimengerti serta kualitas materi nya sangat bagus.</p>
+      </div>
+    </div>
+
+    <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem; height:300px;">
+      <div class="card-header text-center">
+        <div class="row">
+            <div class="col-md-4">
+              <img src="{{ URL('images/robi.jpg') }}" alt="" class="rounded rounded-circle mt-3" style="max-width: 4rem; height:4rem;">
+            </div>
+            <div class="col-md-6 mt-3 ">
+              <p>Robi Nurhidayat</p>
+              <p>Full-Stack Developer</p>
+            </div>
+        </div>
+      </div>
+      <div class="card-body">
+        {{-- <h5 class="card-title">Waktu Fleksibel</h5> --}}
+        <p class="card-text">Penjelasan mudah dimengerti serta kualitas materi nya sangat bagus.</p>
+      </div>
+    </div>
+
+
+    <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem; height:300px;">
+      <div class="card-header text-center">
+        <div class="row">
+            <div class="col-md-4">
+              <img src="{{ URL('images/akbar.jpg') }}" alt="" class="rounded rounded-circle mt-3" style="max-width: 4rem; height:4rem;">
+            </div>
+            <div class="col-md-6 mt-3 ">
+              <p>Akbar Saleh</p>
+              <p>Android Developer</p>
+            </div>
+        </div>
+      </div>
+      <div class="card-body">
+        {{-- <h5 class="card-title">Waktu Fleksibel</h5> --}}
+        <p class="card-text">Penjelasan mudah dimengerti serta kualitas materi nya sangat bagus.</p>
+      </div>
+    </div>
+
+
+    <div class="card text-black mb-3 shadow-lg rounded" style="max-width: 18rem; height:300px;">
+      <div class="card-header text-center">
+        <div class="row">
+            <div class="col-md-4">
+              <img src="{{ URL('images/gaber.jpg') }}" alt="" class="rounded rounded-circle mt-3" style="max-width: 4rem; height:4rem;">
+            </div>
+            <div class="col-md-6 mt-3 ">
+              <p>Adam Alfian</p>
+              <p>IOS Developer</p>
+            </div>
+        </div>
+      </div>
+      <div class="card-body">
+        {{-- <h5 class="card-title">Waktu Fleksibel</h5> --}}
+        <p class="card-text">Penjelasan mudah dimengerti serta kualitas materi nya sangat bagus.</p>
+      </div>
+    </div>
+
+  </div>
+
+  
+
+
+</div>
+
+
+{{-- end of testimonial students --}}
 
 @endsection
